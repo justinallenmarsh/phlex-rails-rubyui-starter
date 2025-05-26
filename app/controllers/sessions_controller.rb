fa531cba042b9b95943bbac6ff47ce320fb1a270
Justin Marsh <jmarsh24@gmail.com>
2025-05-26 15:31:31 +0200

    redirect to dashboard upon successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
       @session = user.sessions.create!
       cookies.signed.permanent[:session_token] = { value: @session.id, httponly: true }
 
-      redirect_to root_path, notice: "Signed in successfully"
+      redirect_to dashboard_path, notice: "Signed in successfully"
     else
       redirect_to sign_in_path, alert: "That email or password is incorrect"
     end
