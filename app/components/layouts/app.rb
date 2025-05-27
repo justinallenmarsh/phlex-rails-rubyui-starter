81d615727b6483ae57786c8f7d45f19f78d30ae9
Justin Marsh <jmarsh24@gmail.com>
2025-05-27 12:48:47 +0200

    first attempt

--- app/components/layouts/app.rb
+++ app/components/layouts/app.rb
@@ -1,16 +1,22 @@
+# frozen_string_literal: true
+
 module Components
   module Layouts
     class App < Base
+      def initialize(page_info)
+        @page_info = page_info
+      end
+
+      def layout = Root
+
       def view_template(&)
-        Layouts.Root do
-          div(class: "flex bg-sidebar") do
-            div(class: "hidden md:block") do
-              Layouts.Sidenav
-            end
-            div(class: "bg-sidebar h-dvh bg-background relative flex w-full flex-1 flex-col lg:pt-2 lg:pb-2 lg:pr-2 md:pl-2") do
-              main(class: "bg-background flex w-full flex-1 flex-col rounded-lg") do
-                  yield
-              end
+        div(class: "flex bg-sidebar") do
+          div(class: "hidden md:block") do
+            Layouts::Sidenav
+          end
+          div(class: "bg-sidebar h-dvh bg-background relative flex w-full flex-1 flex-col lg:pt-2 lg:pb-2 lg:pr-2 md:pl-2") do
+            main(class: "bg-background flex w-full flex-1 flex-col rounded-lg") do
+              yield
             end
           end
         end
--- app/views/base.rb
+++ app/views/base.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
-module Views
-  class Base < Components::Base
+class Views::Base < Components::Base
+  PageInfo = Data.define(:title)
+
+  def around_template
+    render layout.new(page_info) do
+      super
+    end
+  end
+
+  def page_info
+    PageInfo.new(
+      title: page_title
+    )
   end
 end
